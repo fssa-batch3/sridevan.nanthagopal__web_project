79714abc2a08429814b243d547846ae46322fbfc
sridev01 <117353390+sridev01@users.noreply.github.com>
2023-02-10 14:56:16 +0530

    Commit feb10

---
+++ pages/menu.js
@@ -0,0 +1,459 @@
+/* <div class="dishgrid-item">
+<div class="dishgrid-item-img">
+  <img src="../assets/images/vegmeals3.jfif" alt="" />
+</div>
+<div class="dishgrid-item-info">
+  <h3 class="dishgrid-item-title">Veg Meals</h3>
+  <h3 class="dishgrid-item-price">$50</h3>
+  <div class="dishgrid-item-stars">
+    <img src="../assets/images/4starrating.png" alt="" />
+  </div>
+</div>
+</div> */
+
+let top_dishes = [
+  {
+    image: "../assets/images/pexels-saveurs-secretes-5560763.jpg",
+    altImage: "plain-dosa",
+    item_title: "Plain Dosa",
+    item_price: "$50",
+    star_img: "../assets/images/4starrating.png",
+  },
+
+  {
+    image: "../assets/images/Chappathi.jpg",
+    altImage: "Chappathi",
+    item_title: "Chappathi",
+    item_price: "$50",
+    star_img: "../assets/images/4starrating.png",
+  },
+
+  {
+    image: "../assets/images/vegmeals3.jfif",
+    altImage: "vegmeals3",
+    item_title: "Veg Meals",
+    item_price: "$50",
+    star_img: "../assets/images/4starrating.png",
+  },
+];
+
+let n = 2;
+for (let i = 0; i <= n; i++) {
+  let dishgrid_item;
+  dishgrid_item = document.createElement("div");
+  dishgrid_item.setAttribute("class", "dishgrid-item");
+  console.log(dishgrid_item);
+
+  let dishgrid_item_img;
+  dishgrid_item_img = document.createElement("div");
+  dishgrid_item_img.setAttribute("class", "dishgrid-item-img");
+  dishgrid_item.append(dishgrid_item_img);
+
+  let item_img;
+  item_img = document.createElement("img");
+  item_img.setAttribute("src", top_dishes[i]["image"]);
+  item_img.setAttribute("alt", top_dishes[i]["altImage"]);
+  dishgrid_item_img.append(item_img);
+
+  let dishgrid_item_info;
+  dishgrid_item_info = document.createElement("div");
+  dishgrid_item_info.setAttribute("class", "dishgrid-item-info");
+  dishgrid_item.append(dishgrid_item_info);
+
+  let dishgrid_item_title;
+  dishgrid_item_title = document.createElement("h3");
+  dishgrid_item_title.setAttribute("class", "dishgrid-item-title");
+  dishgrid_item_title.innerText = top_dishes[i]["item_title"];
+  dishgrid_item_info.append(dishgrid_item_title);
+
+  let dishgrid_item_price;
+  dishgrid_item_price = document.createElement("h3");
+  dishgrid_item_price.setAttribute("class", "dishgrid-item-price");
+  dishgrid_item_price.innerText = top_dishes[i]["item_price"];
+  dishgrid_item_info.append(dishgrid_item_price);
+
+  let dishgrid_item_stars;
+  dishgrid_item_stars = document.createElement("div");
+  dishgrid_item_stars.setAttribute("class", "dishgrid-item-stars");
+  dishgrid_item_info.append(dishgrid_item_stars);
+
+  let item_star_img;
+  dishgrid_star_img = document.createElement("img");
+  dishgrid_star_img.setAttribute("src", top_dishes[i]["star_img"]);
+  dishgrid_item_stars.prepend(dishgrid_star_img);
+
+  document.querySelector("div.dishgrid-wrapper").append(dishgrid_item);
+}
+// End of top dishes
+
+// Breakfast
+
+let breakfast = [
+  {
+    image: "../assets/images/Idly.jpeg",
+    altImage: "Idly",
+    item_title: "Idly [2pcs]",
+    item_price: "$28",
+    star_img: "../assets/images/4starrating.png",
+  },
+
+  {
+    image:
+      "../assets/images/Instant Dosa From Leftover Rice (easy and crispy dosa) - Cooking Carnival.jfif",
+      altImage: "plain-dosa",
+    item_title: "Plain Roast",
+    item_price: "$62",
+    star_img: "../assets/images/4starrating.png",
+  },
+
+  {
+    image: "../assets/images/oniondosa.jpeg",
+    altImage: "oniondosa",
+    item_title: "Onion Roast",
+    item_price: "$50",
+    star_img: "../assets/images/4starrating.png",
+  },
+  {
+    image: "../assets/images/Uttapam.jpeg",
+    altImage: "Uttapam",
+    item_title: "Uttapam",
+    item_price: "$80",
+    star_img: "../assets/images/3stars-rating.png",
+  },
+  {
+    image: "../assets/images/egg-uthappam.jpeg",
+    altImage: "egg-uthappam",
+    item_title: "Egg Uthapam",
+    item_price: "$70",
+    star_img: "../assets/images/3stars-rating.png",
+  },
+  {
+    image: "../assets/images/podi-dosa.jpeg",
+    altImage: "podi-dosa",
+    item_title: "Podi Dosa",
+    item_price: "$50",
+    star_img: "../assets/images/4starrating.png",
+  },
+  {
+    image: "../assets/images/kaldosa.jpeg",
+    altImage: "kaldosa",
+    item_title: "Kal Dosa",
+    item_price: "$70",
+    star_img: "../assets/images/4starrating.png",
+  },
+  {
+    image: "../assets/images/pongal.jpeg",
+    altImage: "pongal",
+    item_title: "Ven Pongal",
+    item_price: "$30",
+    star_img: "../assets/images/5starrating.png",
+  },
+  {
+    image: "../assets/images/poori.jpeg",
+    altImage: "poori",
+    item_title: "Poori",
+    item_price: "$50",
+    star_img: "../assets/images/3stars-rating.png",
+  },
+];
+
+let m = 8;
+for (let i = 0; i <= m; i++) {
+  let dishgrid_item;
+  dishgrid_item = document.createElement("div");
+  dishgrid_item.setAttribute("class", "dishgrid-item");
+  console.log(dishgrid_item);
+
+  let dishgrid_item_img;
+  dishgrid_item_img = document.createElement("div");
+  dishgrid_item_img.setAttribute("class", "dishgrid-item-img");
+  dishgrid_item.append(dishgrid_item_img);
+
+  let item_img;
+  item_img = document.createElement("img");
+  item_img.setAttribute("src", breakfast[i]["image"]);
+  item_img.setAttribute("alt", breakfast[i]["altImage"]);
+  dishgrid_item_img.append(item_img);
+
+  let dishgrid_item_info;
+  dishgrid_item_info = document.createElement("div");
+  dishgrid_item_info.setAttribute("class", "dishgrid-item-info");
+  dishgrid_item.append(dishgrid_item_info);
+
+  let dishgrid_item_title;
+  dishgrid_item_title = document.createElement("h3");
+  dishgrid_item_title.setAttribute("class", "dishgrid-item-title");
+  dishgrid_item_title.innerText = breakfast[i]["item_title"];
+  dishgrid_item_info.append(dishgrid_item_title);
+
+  let dishgrid_item_price;
+  dishgrid_item_price = document.createElement("h3");
+  dishgrid_item_price.setAttribute("class", "dishgrid-item-price");
+  dishgrid_item_price.innerText = breakfast[i]["item_price"];
+  dishgrid_item_info.append(dishgrid_item_price);
+
+  let dishgrid_item_stars;
+  dishgrid_item_stars = document.createElement("div");
+  dishgrid_item_stars.setAttribute("class", "dishgrid-item-stars");
+  dishgrid_item_info.append(dishgrid_item_stars);
+
+  let item_star_img;
+  dishgrid_star_img = document.createElement("img");
+  dishgrid_star_img.setAttribute("src", breakfast[i]["star_img"]);
+  dishgrid_item_stars.prepend(dishgrid_star_img);
+
+  document
+    .querySelector("div#dishgrid-wrapper-breakfast")
+    .append(dishgrid_item);
+}
+
+// End of top breakfast
+
+// ///////////////////////////////////////////////Lunch//////////////////////////////////
+// Lunch
+let lunch = [
+  {
+    image: "../assets/images/Vegmeals.jpeg",
+    altImage: "Vegmeals",
+    item_title: "Veg Meals",
+    item_price: "$28",
+    star_img: "../assets/images/5starrating.png",
+  },
+
+  {
+    image:
+      "../assets/images/Poratta.jpeg",
+      altImage: "Poratta",
+    item_title: "Porotta",
+    item_price: "$62",
+    star_img: "../assets/images/4starrating.png",
+  },
+
+  {
+    image: "../assets/images/tomatorice.jpg",
+    altImage: "tomatorice",
+    item_title: "Tomato Rice",
+    item_price: "$50",
+    star_img: "../assets/images/4starrating.png",
+  },
+  {
+    image: "../assets/images/Indian Lemon Rice.jfif",
+    altImage: "Indian Lemon Rice",
+    item_title: "Lemon Rice",
+    item_price: "$80",
+    star_img: "../assets/images/3stars-rating.png",
+  },
+  {
+    image: "../assets/images/curd rice.jfif",
+    altImage: "curd-rice",
+    item_title: "Curd Rice",
+    item_price: "$70",
+    star_img: "../assets/images/3stars-rating.png",
+  },
+  {
+    image: "../assets/images/Kothuporatta1.jpeg",
+    altImage: "Kothuporatta1",
+    item_title: "Veg Kothu Parotta",
+    item_price: "$50",
+    star_img: "../assets/images/4starrating.png",
+  },
+  {
+    image: "../assets/images/eggkothu.jpeg",
+    altImage: "eggkothu",
+    item_title: "Egg Kothu Parotta",
+    item_price: "$70",
+    star_img: "../assets/images/4starrating.png",
+  }
+];
+
+// let k = 6;
+for (let i = 0; i <= lunch.length -1; i++) {
+  let dishgrid_item;
+  dishgrid_item = document.createElement("div");
+  dishgrid_item.setAttribute("class", "dishgrid-item");
+  console.log(dishgrid_item);
+
+  let dishgrid_item_img;
+  dishgrid_item_img = document.createElement("div");
+  dishgrid_item_img.setAttribute("class", "dishgrid-item-img");
+  dishgrid_item.append(dishgrid_item_img);
+
+  let item_img;
+  item_img = document.createElement("img");
+  console.log(lunch[i]["image"])
+  item_img.setAttribute("src", lunch[i]["image"]);
+  item_img.setAttribute("alt", lunch[i]["altImage"]);
+  dishgrid_item_img.append(item_img);
+
+  let dishgrid_item_info;
+  dishgrid_item_info = document.createElement("div");
+  dishgrid_item_info.setAttribute("class", "dishgrid-item-info");
+  dishgrid_item.append(dishgrid_item_info);
+
+  let dishgrid_item_title;
+  dishgrid_item_title = document.createElement("h3");
+  dishgrid_item_title.setAttribute("class", "dishgrid-item-title");
+  dishgrid_item_title.innerText = lunch[i]["item_title"];
+  dishgrid_item_info.append(dishgrid_item_title);
+
+  let dishgrid_item_price;
+  dishgrid_item_price = document.createElement("h3");
+  dishgrid_item_price.setAttribute("class", "dishgrid-item-price");
+  dishgrid_item_price.innerText = lunch[i]["item_price"];
+  dishgrid_item_info.append(dishgrid_item_price);
+
+  let dishgrid_item_stars;
+  dishgrid_item_stars = document.createElement("div");
+  dishgrid_item_stars.setAttribute("class", "dishgrid-item-stars");
+  dishgrid_item_info.append(dishgrid_item_stars);
+
+  let item_star_img;
+  dishgrid_star_img = document.createElement("img");
+  dishgrid_star_img.setAttribute("src", lunch[i]["star_img"]);
+  dishgrid_item_stars.prepend(dishgrid_star_img);
+
+  document.querySelector("div#dishgrid-wrapper-lunch").append(dishgrid_item);
+}
+
+// Dinner//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let dinner = [
+  {
+    image: "../assets/images/Idly.jpeg",
+    altImage: "Idly",
+    item_title: "Idly [2pcs]",
+    item_price: "$28",
+    star_img: "../assets/images/4starrating.png",
+  },
+
+  {
+    image:
+      "../assets/images/Instant Dosa From Leftover Rice (easy and crispy dosa) - Cooking Carnival.jfif",
+      altImage: "plain-dosa",
+    item_title: "Plain Roast",
+    item_price: "$62",
+    star_img: "../assets/images/4starrating.png",
+  },
+
+  {
+    image: "../assets/images/oniondosa.jpeg",
+    altImage: "oniondosa",
+    item_title: "Onion Roast",
+    item_price: "$50",
+    star_img: "../assets/images/4starrating.png",
+  },
+  {
+    image: "../assets/images/Uttapam.jpeg",
+    altImage: "Uttapam",
+    item_title: "Uttapam",
+    item_price: "$80",
+    star_img: "../assets/images/3stars-rating.png",
+  },
+  {
+    image: "../assets/images/egg-uthappam.jpeg",
+    altImage: "egguthappam",
+    item_title: "Egg Uthapam",
+    item_price: "$70",
+    star_img: "../assets/images/3stars-rating.png",
+  },
+  {
+    image: "../assets/images/podi-dosa.jpeg",
+    altImage: "podidosa",
+    item_title: "Podi Dosa",
+    item_price: "$50",
+    star_img: "../assets/images/4starrating.png",
+  },
+  {
+    image: "../assets/images/kaldosa.jpeg",
+    altImage: "kaldosa",
+    item_title: "Kal Dosa",
+    item_price: "$70",
+    star_img: "../assets/images/4starrating.png",
+  },
+ 
+  {
+    image: "../assets/images/Kothuporatta1.jpeg",
+    altImage: "Kothuporatta1",
+    item_title: "Veg Kothu Parotta",
+    item_price: "$30",
+    star_img: "../assets/images/5starrating.png",
+  },
+  {
+    image: "../assets/images/eggkothu.jpeg",
+    altImage: "eggkothu",
+    item_title: "Egg Kothu Parotta",
+    item_price: "$50",
+    star_img: "../assets/images/3stars-rating.png",
+  },
+  {
+    image: "../assets/images/Chappathi.jpg",
+    altImage: "Chappathi",
+    item_title: "Chappathi",
+    item_price: "$50",
+    star_img: "../assets/images/3stars-rating.png",
+  },
+  {
+    image: "../assets/images/vegkothuchappathi.jpeg",
+    altImage: "vegkothuchappathi",
+    item_title: "Kothu Chappathi",
+    item_price: "$50",
+    star_img: "../assets/images/3stars-rating.png",
+  },
+  {
+    image: "../assets/images/eggkothuchappathi.jpeg",
+    altImage: "eggkothuchappathi",
+    item_title: "Egg Chappathi",
+    item_price: "$50",
+    star_img: "../assets/images/3stars-rating.png",
+  },
+];
+
+
+for (let i = 0; i <= dinner.length -1; i++) {
+  let dishgrid_item;
+  dishgrid_item = document.createElement("div");
+  dishgrid_item.setAttribute("class", "dishgrid-item");
+  console.log(dishgrid_item);
+
+  let dishgrid_item_img;
+  dishgrid_item_img = document.createElement("div");
+  dishgrid_item_img.setAttribute("class", "dishgrid-item-img");
+  dishgrid_item.append(dishgrid_item_img);
+
+  let item_img;
+  item_img = document.createElement("img");
+  item_img.setAttribute("src", dinner[i]["image"]);
+  item_img.setAttribute("alt", dinner[i]["altImage"]);
+  item_img.setAttribute("alt" , "")
+  dishgrid_item_img.append(item_img);
+
+  let dishgrid_item_info;
+  dishgrid_item_info = document.createElement("div");
+  dishgrid_item_info.setAttribute("class", "dishgrid-item-info");
+  dishgrid_item.append(dishgrid_item_info);
+
+  let dishgrid_item_title;
+  dishgrid_item_title = document.createElement("h3");
+  dishgrid_item_title.setAttribute("class", "dishgrid-item-title");
+  dishgrid_item_title.innerText = dinner[i]["item_title"];
+  dishgrid_item_info.append(dishgrid_item_title);
+
+  let dishgrid_item_price;
+  dishgrid_item_price = document.createElement("h3");
+  dishgrid_item_price.setAttribute("class", "dishgrid-item-price");
+  dishgrid_item_price.innerText = dinner[i]["item_price"];
+  dishgrid_item_info.append(dishgrid_item_price);
+
+  let dishgrid_item_stars;
+  dishgrid_item_stars = document.createElement("div");
+  dishgrid_item_stars.setAttribute("class", "dishgrid-item-stars");
+  dishgrid_item_info.append(dishgrid_item_stars);
+
+  let item_star_img;
+  dishgrid_star_img = document.createElement("img");
+  dishgrid_star_img.setAttribute("src", dinner[i]["star_img"]);
+  dishgrid_item_stars.prepend(dishgrid_star_img);
+
+  document.querySelector("div#dishgrid-wrapper-dinner").append(dishgrid_item);
+}
---
+++ reviews.js
@@ -0,0 +1,126 @@
+
+
+let reviews = [
+  {
+    image: "./assets/images/maheshwarpng.png",
+    altImage: "maheshwarpng",
+    review_title: "Maheswaran Shivam",
+    review_text:
+      "The tamizhan hotel not just a hotel... Every time when we had food in there, we feel completely feel very homely... Bcoz, they made the food very delicious and taste... Last four years they care us like our mom...Thank god... ",
+    star_img: "../assets/images/4starrating.png",
+    star_img_alt: "4star"
+  },
+  {
+    image: "./assets/images/periyasamyreview.png",
+    altImage: "periyasamyreview",
+    review_title: "Periya Samy",
+    review_text:
+      "This hotel is totally vegetarian . It is present near the A.P.A collage of arts and culture and A.P.A polytechnic college . Food idems are good and tasty . check it now .",
+    star_img: "./assets/images/3stars-rating.png",
+    star_img_alt: "3star"
+  },
+  {
+    image: "./assets/images/sureshreviewpng.png",
+    altImage: "sureshreviewpng",
+    review_title: "Suresh Kumar",
+    review_text:
+      "one of the better hotel in local town in that college surrounding area.. cheap and better..",
+    star_img: "./assets/images/4starrating.png",
+    star_img_alt: "4star"
+  },
+  {
+    image: "./assets/images/vijayreview.png",
+    altImage: "vijayreview",
+    review_title: "Vijayashankar Aruchamy",
+    review_text:
+      "Parota are good here, onion dosa my fab",
+    star_img: "./assets/images/4starrating.png",
+    star_img_alt: "4star"
+  },
+];
+
+
+
+
+
+let n = 3;
+for (let i = 0; i <= n; i++) {
+{
+  /* <div class="testimonial-item"> */
+}
+let review_item;
+review_item = document.createElement("div");
+review_item.setAttribute("class", "testimonial-item");
+console.log(review_item);
+
+{
+  /* <div class="testimonial-item-img"> */
+}
+let review_item_img;
+review_item_img = document.createElement("div");
+review_item_img.setAttribute("class", "testimonial-item-img");
+review_item.append(review_item_img);
+
+{
+  /* <img class="reviwer-img" src="./assets/images/maheshwarpng.png"alt=""/> */
+}
+
+let reviwer_img;
+reviwer_img = document.createElement("img");
+reviwer_img.setAttribute("class", "reviwer-img");
+reviwer_img.setAttribute("src", reviews[i]["image"]);
+reviwer_img.setAttribute("alt", reviews[i]["altImage"]);
+review_item_img.append(reviwer_img);
+
+{
+  /* <div class="testimonial-item-info"> */
+}
+
+let review_item_info;
+review_item_info = document.createElement("div");
+review_item_info.setAttribute("class", "testimonial-item-info");
+review_item.append(review_item_info);
+
+{
+  /* <h3 class="testimonial-item-name">Maheswaran Shivam</h3> */
+}
+
+let review_item_name;
+review_item_name = document.createElement("h3");
+review_item_name.setAttribute("class", "testimonial-item-name");
+review_item_name.innerText = reviews[i]["review_title"];
+review_item_info.append(review_item_name);
+
+{
+  /* <div class="testimonial-item-stars"> */
+}
+
+let review_item_stars;
+review_item_stars = document.createElement("div");
+review_item_stars.setAttribute("class", "testimonial-item-stars");
+review_item_info.append(review_item_stars);
+
+{
+  /* <img src="./assets/images/5starrating.png" alt="" /> */
+}
+
+let review_item_stars_img;
+review_item_stars_img = document.createElement("img");
+review_item_stars_img.setAttribute("src", reviews[i]["star_img"]);
+review_item_stars_img.setAttribute("alt", reviews[i]["star_img_alt"]);
+review_item_stars.append(review_item_stars_img);
+
+{
+  /* <p class="testimonial-item-text"> */
+}
+
+let review_item_text;
+review_item_text = document.createElement("p");
+review_item_text.setAttribute("class", "testimonial-item-text");
+review_item_text.innerText = reviews[i]
+  ["review_text"];
+review_item_info.append(review_item_text);
+
+document.querySelector("div.testimoninal-items-wrapper").append(review_item);
+
+}
